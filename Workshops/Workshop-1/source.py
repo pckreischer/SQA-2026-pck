28515ca27cf73b9e37666a2e6c7f26f42d1476b2
Paul <pckreischer5@gmail.com>
2026-02-25 20:45:11 -0600

    blah

--- Workshops/Workshop-1/source.py
+++ Workshops/Workshop-1/source.py
@@ -1,3 +1,6 @@
+# Paul Kreischer - pck0010@auburn.edu
+# SQA - Workshop 1
+
 def performSub(a, b):
 
     return a-b
@@ -5,3 +8,19 @@ def performSub(a, b):
 def performAdd(a, b):
 
     return a+b
+
+def performMul(a, b):
+
+    return a*b
+
+def performDiv(a, b):
+
+    if b == 0:
+        return "Error: Divide by zero"
+    return a/b
+
+def performSqrt(a):
+
+    if a < 0:
+        return "Error: Square root of negative number"
+    return a**0.5
--- Workshops/Workshop-1/test.py
+++ Workshops/Workshop-1/test.py
@@ -1,23 +1,40 @@
+# Paul Kreischer - pck0010@auburn.edu
+# SQA - Workshop 1
+
 import unittest
 import source  # type: ignore
 
-
+# odd numbered tests pass, even numbered tests fail
 class TestCalc(unittest.TestCase):
     def testSub1(self):
-        self.assertEqual(1, source.performSub(3, 2), "Bug in code, check please!")
-        
+        self.assertEqual(1, source.performSub(3, 2), "Error in subtraction function")
 
     def testSub2(self):
-        self.assertEqual(4, source.performSub(6, 2), "Bug in code, check please!")
+        self.assertEqual(5, source.performSub(6, 2), "Error in subtraction function")
         
     def testAdd1(self):
-        self.assertEqual(4, source.performAdd(1, 3), "Bug in code, check please!")
+        self.assertEqual(4, source.performAdd(1, 3), "Error in addition function")
         
-    def testAdd1(self):
-        self.assertEqual(4, source.performAdd(1, 3), "Bug in code, check please!")
+    def testAdd2(self):
+        self.assertEqual(9, source.performAdd(5, 3), "Error in addition function")
         
+    def testMul1(self):
+        self.assertEqual(6, source.performMul(2, 3), "Error in multiplication function")
+
+    def testMul2(self):
+        self.assertEqual(16, source.performMul(5, 3), "Error in multiplication function")
+
+    def testDiv1(self):
+        self.assertEqual(2, source.performDiv(6, 3), "Error in division function")
+
+    def testDiv2(self):
+        self.assertEqual(1, source.performDiv(5, 0), "Error in division function")
     
-   
+    def testSqrt1(self):
+        self.assertEqual(4, source.performSqrt(16), "Error in square root function")
+
+    def testSqrt2(self):
+        self.assertEqual(1, source.performSqrt(-9), "Error in square root function")
    
 if __name__ == '__main__': 
     unittest.main()
